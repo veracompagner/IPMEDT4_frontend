f5b3fba8ec3422d9842cb30b0c3b1cbf07c57ef4
Zissely van Bercum <zisvanbercum@hotmail.com>
2019-05-27 13:50:56 +0200

    Rewrite of login/register component
    To start with the routing a rewrite of the login/register component to show or hide the right boxes.
    Changed the usage of app.js to only link to the right components.

--- src/Login.js
+++ src/Login.js
@@ -3,38 +3,129 @@ import "./Form.css";
 
 class Login extends React.Component {
 
+    /**
+     * @func constructor
+     * @param props
+     * This function initializes the states.
+     * Default shows the login boxes.
+     * state has:
+     * isLogin = show login
+     * isRegister = show register
+     **/
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isLogin: true,
+            isRegister: false
+        };
+    }
+    /**
+     * @func showLogin
+     * @func showRegister
+     *
+     * Two functions to change the states of isLogin and isRegister
+     * These are used to hide/show the right divs
+     **/
+
+    showLogin() {
+        this.setState({isLogin: true, isRegister: false});
+    }
+    showRegister() {
+        this.setState({isRegister: true, isLogin: false});
     }
 
+
+    /**
+     * @func submitLogin
+     * @param e the event
+     * Submits the form
+     **/
     submitLogin(e) {}
 
     render() {
         return (
             <div>
+                <img className="logoImg" src="./img/appLogo.png" alt="appLogo"></img>
+
+                <div className="loginRegisterBox">
+                    <button
+                        className={"loginRegister " + (this.state.isLogin ? "loginRegisterActive": "")}
+                        onClick={this.showLogin.bind(this)}>Login
+                    </button>
+
+                    <button
+                        className={"loginRegister " + (this.state.isRegister ? "loginRegisterActive": "")}
+                        onClick={this.showRegister.bind(this)}>Account Maken
+                    </button>
+                </div>
 
                 {/* input username */}
-                <div className="input">
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Gebruikersnaam"/>
+                <div className={"LoginInputStuffs " + (this.state.isLogin ? "showstuffs": "hidestuffs")}>
+                    <div className="input">
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Gebruikersnaam"/>
+                    </div>
+
+                    {/* input password */}
+                    <div className="input">
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Wachtwoord"/>
+                    </div>
+
+                    {/* submit button */}
+                    <button
+                        type="button"
+                        className="loginButton"
+                        onClick={this.submitLogin.bind(this)}>Login
+                    </button>
+                </div>
+                <div className={"RegisterInputStuffs " + (this.state.isRegister ? "showstuffs": "hidestuffs")}>
+                    {/* input username */}
+                    <div className="input">
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Gebruikersnaam"/>
+                    </div>
+
+                    {/* input mail */}
+                    <div className="input">
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Email"/>
+                    </div>
+
+
+                    {/* input password */}
+                    <div className="input">
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Wachtwoord"/>
+                    </div>
+
+                    <div className="input">
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            placeholder="Bevestig Wactwoord"/>
+                    </div>
+
+                    {/* submit button */}
+                    <button
+                        type="button"
+                        className="loginButton"
+                        onClick={this.submitLogin.bind(this)}>Registreer</button>
                 </div>
 
-                {/* input password */}
-                <div className="input">
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Wachtwoord"/>
-                </div>
 
-                {/* submit button */}
-                <button
-                    type="button"
-                    className="loginButton"
-                    onClick={this.submitLogin.bind(this)}>Login</button>
+
+
 
             </div>
         );
