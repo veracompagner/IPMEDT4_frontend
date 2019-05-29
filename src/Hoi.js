7b701f2da3e6966fb10f90872611eb31cec8c6bb
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-05-29 14:08:26 +0200

    Added functional components

--- src/Hoi.js
+++ src/Hoi.js
@@ -4,9 +4,14 @@ import React from "react";
  * This class is just a placeholder, needs to be changed when people
  * work on routing/different screens. This just shows stuff works.
  **/
-const Hoi = () => {
+const Hoi = props => {
     return (
-        <p>Hallo!!</p>
+        <div>
+            <p>Hallo!!</p>
+            <button onClick={this.props.logoutUser}>
+                Logout
+            </button>
+        </div>
     )
 }
 
