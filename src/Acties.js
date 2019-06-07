dd56b3c5b8782bdbd977a002053de1d84e52d6b1
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-07 12:42:22 +0200

    Add back icon to acties paga

--- src/Acties.js
+++ src/Acties.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Acties.scss";
 
@@ -7,6 +8,7 @@ import example from "./img/exampleComp.jpg";
 const Acties = (props) => {
     return (
         <div id="acties">
+            <Link to="/"><i class="material-icons">arrow_back</i></Link>
             {/* Users current amount of points */}
             <p id="personalPoints" className="points">{props.punten || 0} Punten</p>
 
