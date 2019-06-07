74501d79b2567e9079be2098d19660ee7063b5b0
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-07 11:30:03 +0200

    Add link to acties page on dashboard

--- src/Hoi.js
+++ src/Hoi.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 /**
  * This class is just a placeholder, needs to be changed when people
  * work on routing/different screens. This just shows stuff works.
@@ -11,6 +13,10 @@ const Hoi = props => {
             <button onClick={props.logoutUser}>
                 Logout
             </button>
+            <p>
+                <Link to="/acties">Acties pagina</Link>
+            </p>
+
         </div>
     )
 }
