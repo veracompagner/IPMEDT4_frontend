05fc10d65a40e03b7b31ca69e26ae23db6bea48f
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-07 10:40:10 +0200

    Change acties to functional component

--- src/Acties.js
+++ src/Acties.js
@@ -4,34 +4,26 @@ import "./Acties.scss";
 
 import example from "./img/exampleComp.jpg";
 
-class Acties extends React.Component {
+const Acties = () => {
+    return (
+        <div>
+            {/* Users current amount of points */}
+            <p id="personalPoints" className="points">(Not received)</p>
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+            {/* Copany image pathname + exchangeable product name + company name + needed amount of points */}
+            <div className="pointsExchangeCard">
+                <div class="companyLogoWrapper">
+                    <img className="companyLogo" src={example} alt="(Company name) Logo"></img>
+                </div>
 
-    render() {
-        return (
-            <div>
-                {/* Users current amount of points */}
-                <p id="personalPoints" className="points">(Not received)</p>
-
-                {/* Copany image pathname + exchangeable product name + company name + needed amount of points */}
-                <div className="pointsExchangeCard">
-                    <div class="companyLogoWrapper">
-                        <img className="companyLogo" src={example} alt="(Company name) Logo"></img>
-                    </div>
-
-                    <div className="exchangeCardTextInfoContainer">
-                        <h1 className="companyName infoText">Example Comp.</h1>
-                        <p className="productDescription infoText">GRATIS Product</p>
-                        <p className="cardPoints points infoText">5000</p>
-                    </div>
+                <div className="exchangeCardTextInfoContainer">
+                    <h1 className="companyName infoText">Example Comp.</h1>
+                    <p className="productDescription infoText">GRATIS Product</p>
+                    <p className="cardPoints points infoText">5000</p>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Acties;
