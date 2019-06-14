6d15173e31a841328ff7d0a3a70ec73d7ff44b7b
Zissely van Bercum <zisvanbercum@hotmail.com>
2019-06-14 12:48:53 +0200

    register Alert bar for errors
    Added a red line of text showing register errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 
 import axios from "axios";
@@ -7,6 +8,7 @@ import Login from "./Login";
 import Register from "./Register";
 import Hoi from "./Hoi";
 import Acties from "./Acties";
+import Alert from "./Alert";
 
 import "./App.scss";
 
@@ -25,11 +27,11 @@ class App extends React.Component {
             user: {}
         };
         this.apiurl = "http://" + window.location.hostname + ":8000/api";
+        this.modalroot = document.getElementById('modal-root');
         // this.apiurl = "http://IPMEDT4_api.test/api"; Meuk voor matthijs
     }
 
     loginUser = (email, password) => {
-
         var formData = new FormData();
         formData.append("email", email);
         formData.append("password", password);
@@ -63,7 +65,6 @@ class App extends React.Component {
     };
 
     registerUser = (name, email, password, password_confirmation) => {
-
         var formData = new FormData();
 
         formData.append("name", name);
@@ -91,9 +92,32 @@ class App extends React.Component {
                         user: appState.user
                     });
                 } else console.log(`Registration Failed!`);
-            })
+            }
+
+            )
             .catch(error => {
-                console.log("An Error Occured!" + error);
+                //Errorhandling, taking a JSON list and then translating to Dutch
+                //Shows most important errors first (email, password,name)
+                var registerErrors = JSON.parse(error.response.data);
+                var melding = "";
+                if(registerErrors['email']){
+                    if(registerErrors['email'].includes("The email must be a valid email address.")){
+                        melding = "Voer een geldig e-mailadres in."
+                    }else if(registerErrors['email'].includes("The email has already been taken.")){
+                        melding = "Dit e-mailadres bestaat al."
+                    }
+                }else if(registerErrors['password']){
+                    if(registerErrors['password'].includes("The password must be at least 6 characters.")){
+                        melding = "Het wachtwoord moet minimaal 6 karakters lang zijn."
+                    }else if(registerErrors['password'].includes("The password confirmation does not match.")){
+                        melding = "De wachtwoorden komen niet overeen."
+                    }
+                }else if(registerErrors['name'].includes("The name field is required.")){
+                    melding = "Er moet een gebruikersnaam opgegeven worden."
+                }else{
+                    melding = "Er is iets fout gegaan met registreren, check uw gegevens en probeer opnieuw."
+                }
+                ReactDOM.render(<Alert foutmeldingen={melding}></Alert>, document.getElementById('modal-root'));
             });
     };
 
@@ -116,8 +140,7 @@ class App extends React.Component {
                 user: AppState.user
             });
         }
-    }
-
+    };
     render(){
         if (!this.state.isLoggedIn && this.props.location.pathname !== "/login" && this.props.location.pathname !== "/register") {
             return <Redirect to="/login" />;
