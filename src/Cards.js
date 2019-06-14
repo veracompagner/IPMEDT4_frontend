d44a21da02e67c9846a1c43b47c3fa2426e0cb84
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-14 15:26:03 +0200

    Fix all the formatting

--- src/Cards.js
+++ src/Cards.js
@@ -5,17 +5,14 @@ import "./Cards.scss";
 function Cards(props){
 
     return (
-      <div className="cards-root">
-        <img className="cards-img" src={props.img} alt="appLogo"></img>
-        <div className="cards-text">
-
-          <h2> {props.title || "titel"}</h2>
-          {props.text || "Geen tekst beschikbaar"}
+        <div className="cards-root">
+            <img className="cards-img" src={props.img} alt="appLogo"></img>
+            <div className="cards-text">
+                <h2> {props.title || "titel"}</h2>
+                {props.text || "Geen tekst beschikbaar"}
+            </div>
         </div>
-
-  
-      </div>
-    ); 
+    );
 }
 
 export default Cards;
