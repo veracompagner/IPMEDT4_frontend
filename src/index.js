4b756caa51f4ccbf120fb505e37a5c3763d33585
charroite <charlaineruiterman@gmail.com>
2019-06-14 10:05:14 +0200

    Vergeten index js terug te zetten

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-import Overzicht from "./Overzicht";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 
 
-// ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.querySelector("#root"));
-ReactDOM.render(<Overzicht/>, document.querySelector("#root"));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.querySelector("#root"));
+ReactDOM.render(<App/>, document.querySelector("#root"));
