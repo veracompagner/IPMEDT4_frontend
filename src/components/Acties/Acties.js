d507b73e4eef9505cb357496934c8949ae6a5fe9
QuentenZ <quentenzandvliet@me.com>
2019-06-21 12:12:11 +0200

    acties pagina user punten
    de user points aan de props toegevoegd zodat deze onder anderen op de acties pagina getoond kunnen worden.

--- src/components/Acties/Acties.js
+++ src/components/Acties/Acties.js
@@ -1,6 +1,7 @@
 // Import React and the Link component from React-Router
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 // Import SCSS
 import "./Acties.scss";
@@ -18,11 +19,18 @@ const Acties = props => (
         <Link to="/"><i className="material-icons">arrow_back</i></Link>
 
         {/* Users current amount of points, when none are supplied defaults to 0 */}
-        <p id="personalPoints" className="points">{props.punten || 0} Punten</p>
+        <p id="personalPoints" className="points">{props.user.points || 0} Punten</p>
+        { console.log(props.user) }
 
         {/* Company image + exchangeable product name + company name + needed amount of points */}
         <Card img={example} title="Example Comp." text="GRATIS Product" points="5000"/>
     </div>
 );
 
-export default Acties;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(Acties);
