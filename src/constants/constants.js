f5b3fe073246d623b98ca01b3cbe9c65c92574d7
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-21 14:29:33 +0200

    Add production url and add comments

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,4 @@
-export const APIURL = "http://" + window.location.hostname + ":8000/api";
+// Dev URL for php artisan serve:
+export const APIURL = "http://" + window.location.hostname + ":8000";
+// Production URL for apache2:
+// export const APIURL = "https://api." + window.location.hostname;
