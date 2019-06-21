45916a6e4449a4a1957be627c86ddc8afcc4d49b
Zissely van Bercum <zisvanbercum@hotmail.com>
2019-06-21 12:06:43 +0200

    fixed logout back to work

--- src/components/Auth/Logout.js
+++ src/components/Auth/Logout.js
@@ -1,19 +1,10 @@
 import React from "react";
 
-import "./Logout.scss";
-
-import logoutimg from "../../img/logout.png";
-
-
-const Logout = ({logoutUser}) => {
-    function handleLogout(e){
-        e.preventDefault();
-        logoutUser();
-    }
+const Logout = ({ logoutUser }) => {
 
     return (
         <div id="logout">
-            <img className="logoutimg" onClick={handleLogout} src={logoutimg} alt="uitloggen"/>
+            {logoutUser()}
         </div>
     );
 };
