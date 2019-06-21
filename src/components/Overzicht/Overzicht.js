90aaf1a044876de4b547fe109b163e50d9ca97eb
Zissely van Bercum <zisvanbercum@hotmail.com>
2019-06-21 12:07:38 +0200

    Changed img to material-icons

--- src/components/Overzicht/Overzicht.js
+++ src/components/Overzicht/Overzicht.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 import Card from "../Card"
 import Ripple from './Ripple'
 
 import 'material-design-icons/iconfont/material-icons.css';
-
 import "./Overzicht.scss"
 
 import logoZonder from "../../img/appLogoZonder.png";
@@ -14,6 +15,9 @@ class Overzicht extends React.Component {
     render(){
         return(
             <div >
+                <Link to="/auth/logout">
+                    <i className="material-icons">power_settings_new</i>
+                </Link>
                 <div className="overzicht-punten-root">
                     <img className="overzicht-punten-img" src={logoZonder} alt="appLogo"></img>
                     <p className="overzicht-punten-text">0000</p>
@@ -27,14 +31,18 @@ class Overzicht extends React.Component {
                     />
                 </div>
                 <div className="overzicht-footer">
-                    <Ripple
-                        icon="favorite"
-                        name="Acties"
-                    ></Ripple>
-                    <Ripple
-                        icon="location_on"
-                        name="Vertraging"
-                    ></Ripple>
+                    <Link to="/acties">
+                        <Ripple
+                            icon="favorite"
+                            name="Acties"
+                        ></Ripple>
+                    </Link>
+                    <Link to="/auth/register">
+                        <Ripple
+                            icon="location_on"
+                            name="vertraging"
+                        ></Ripple>
+                    </Link>
                 </div>
             </div>
         )
