56a5996462409f78eef09fb145c609abf614b5e8
QuentenZ <quentenzandvliet@me.com>
2019-06-24 13:18:43 +0200

    acties pagina producten ophalen
    Haalt de producten op vanaf de API en toont deze in "card" componenten op de acties pagina.

--- src/components/Acties/Acties.js
+++ src/components/Acties/Acties.js
@@ -1,7 +1,10 @@
 // Import React and the Link component from React-Router
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+
+import { APIURL } from "../../constants/constants";
 
 // Import SCSS
 import "./Acties.scss";
@@ -10,26 +13,45 @@ import "./Acties.scss";
 import Card from "../Card";
 
 // Import Images
-import example from  "../../img/default.png";
-//Test elements till the products prop has been made;
-const elements = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+import example from "../../img/default.png";
+
 // Define functional component
-const Acties = props => (
-    <div id="acties">
-        <div id="header">
-            {/* Back arrow */}
-            <Link to="/"><i className="material-icons arrow icon-left-corner">arrow_back</i></Link>
+class Acties extends React.Component {
+  constructor(props) {
+    super(props);
+    this.retrieveProducts();
+  }
 
-            {/* Users current amount of points, when none are supplied defaults to 0 */}
-            <p id="personalPoints" className="points">{props.user.points || 0} Punten</p>
+  render(){
+      return(
+        <div id="acties">
+            <div id="header">
+                {/* Back arrow */}
+                <Link to="/"><i className="material-icons arrow icon-left-corner">arrow_back</i></Link>
+
+                {/* Users current amount of points, when none are supplied defaults to 0 */}
+                <p id="personalPoints" className="points">{this.props.user.points || 0} Punten</p>
+                {JSON.parse(localStorage["products"]).map((product, index) => {
+                  return <Card img={example} title={product.product} text={product.product} points={product.cost} key={product.id}/>
+                })}
+            </div>
         </div>
+      )
+  }
 
-        {/* Company image + exchangeable product name + company name + needed amount of points */}
-        {elements.map((value, index) => {
-          return <Card img={example} title={value} text="GRATIS Product" points="5000" key={index}/>
-        })}
-    </div>
-);
+  retrieveProducts = () => {
+    axios.get(APIURL + "/acties", {headers: {Authorization: `Bearer ${this.props.user.auth_token}`}})
+      .then(json => {
+          if (json.data.products) {
+              // save products data in local storage
+              localStorage["products"] = JSON.stringify(json.data.products);
+          } else console.log("Retrieving products failed!");
+      })
+      .catch(error => {
+          console.log("Er is iets mis gegaan");
+      });
+  };
+}
 
 const mapStateToProps = state => {
     return {
