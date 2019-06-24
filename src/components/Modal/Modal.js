34cac44d1fc84e102eea9218eaae39ce0550ddf6
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-24 11:27:37 +0200

    Initial cleanup modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-import './Modal.css';
+import { QRCode } from "react-qr-svg";
 
-import QRcode from "../Acties/QRcode";
+import './Modal.scss';
 
 const modal = (props) => {
     return (
         <div>
-            <div className="modalWrapper"
+            <div className="modal"
                 style={{
-                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                    transform: props.show ? 'translateY(0vh)' : 'translateY(-200vh)',
                 }}>
-                <div className="modalBody">
-                    <QRcode value ="hey"/>
+                <div className="modal-body">
+                    <QRCode className="modal-qr" shape-rendering="crispEdges">{ "Hoi" || "QRCode" }</QRCode>
                 </div>
-                <div className="modalFooter">
-                    <button className="cancelButton" onClick={props.close}>CANCEL</button>
-                    <button className="confirmButton">CONFIRM</button>
+                <div className="modal-footer">
+                    <button className="modal-button modal-button-cancel" onClick={props.close}>CANCEL</button>
+                    <button className="modal-button modal-button-confirm">CONFIRM</button>
                 </div>
             </div>
         </div>
