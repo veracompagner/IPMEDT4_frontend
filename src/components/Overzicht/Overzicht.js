b8f6e8932d0d5c0472eeef11769e59b3659e9cbc
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-25 15:30:35 +0200

    Change Overzicht to functional component

--- src/components/Overzicht/Overzicht.js
+++ src/components/Overzicht/Overzicht.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-
 import Card from "../Card"
 import Ripple from './Ripple'
 
@@ -10,40 +9,38 @@ import "./Overzicht.scss"
 import logoZonder from "../../img/appLogoZonder.png";
 import smullers from "../../img/smullers.jpeg";
 
-class Overzicht extends React.Component {
-    render(){
-        return(
-            <div >
-                <Link to="/auth/logout">
-                    <i className="material-icons icon-left-corner">power_settings_new</i>
-                </Link>
-                <div className="overzicht-punten-root">
-                    <img className="overzicht-punten-img" src={logoZonder} alt="appLogo"></img>
-                    <p className="overzicht-punten-text">0000</p>
-                </div>
-                <div className="overzicht-cards">
-                    <Card
-                        title="Gratis Snack"
-                        text="Smullers"
-                        points="2000"
-                        img={smullers}
-                    />
-                </div>
-                <div className="overzicht-footer">
-                    <Link to="/acties">
-                        <Ripple
-                            name="Acties"
-                        ></Ripple>
-                    </Link>
-                    <Link to="/vertraging">
-                        <Ripple
-                            name="vertraging"
-                        ></Ripple>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
-}
+const Overzicht = () => (
+    <div className="overzicht">
+        <Link to="/auth/logout">
+            <i className="material-icons icon-left-corner">power_settings_new</i>
+        </Link>
+        <div className="overzicht-punten-root">
+            <img className="overzicht-punten-img" src={logoZonder} alt="appLogo"></img>
+            <p className="overzicht-punten-tekst">0000</p>
+        </div>
+        <div className="overzicht-cards">
+            <Card
+                title="Gratis Snack"
+                text="Smullers"
+                points="2000"
+                img={smullers}
+            />
+        </div>
+        <div className="overzicht-footer">
+            <Link to="/acties">
+                <Ripple
+                    name="Acties"
+                    icon="favorite"
+                ></Ripple>
+            </Link>
+            <Link to="/vertraging">
+                <Ripple
+                    name="vertraging"
+                    icon="location_on"
+                ></Ripple>
+            </Link>
+        </div>
+    </div>
+)
 
 export default Overzicht;
