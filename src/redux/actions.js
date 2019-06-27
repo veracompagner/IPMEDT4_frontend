d4a06d34a3012be1023bb3339bc803fa5fb96c52
Matthijs van der Meulen <matthijsvdmeulen@gmail.com>
2019-06-28 00:11:30 +0200

    Implement redux store for favorite

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,7 @@ export const CHANGE_ISLOGGEDIN = "CHANGE_ISLOGGEDIN";
 export const CHANGE_USER = "CHANGE_USER";
 export const CHANGE_TOKEN = "CHANGE_TOKEN";
 export const CHANGE_PRODUCTS = "CHANGE_PRODUCTS";
+export const CHANGE_FAVORITE = "CHANGE_FAVORITE";
 
 export const changeIsLoggedIn = isLoggedIn => ({
     type: CHANGE_ISLOGGEDIN,
@@ -20,5 +21,10 @@ export const changeToken = token => ({
 
 export const changeProducts = token => ({
     type: CHANGE_PRODUCTS,
+    payload: token
+});
+
+export const changeFavorite = token => ({
+    type: CHANGE_FAVORITE,
     payload: token
 });
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { CHANGE_ISLOGGEDIN, CHANGE_USER, CHANGE_TOKEN, CHANGE_PRODUCTS } from "./actions.js";
+import { CHANGE_ISLOGGEDIN, CHANGE_USER, CHANGE_TOKEN, CHANGE_PRODUCTS, CHANGE_FAVORITE } from "./actions.js";
 
 export const isLoggedIn = (state = false, action) => {
     switch (action.type) {
@@ -30,6 +30,15 @@ export const token = (state = "", action) => {
 export const products = (state = [], action) => {
     switch (action.type) {
         case CHANGE_PRODUCTS:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export const favorite = (state = 1, action) => {
+    switch (action.type) {
+        case CHANGE_FAVORITE:
             return action.payload;
         default:
             return state;
