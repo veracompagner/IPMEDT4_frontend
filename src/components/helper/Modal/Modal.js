14ac71ed827b06b4634e57284297218c2af9ba45
Zissely van Bercum <zisvanbercum@hotmail.com>
2019-06-28 12:08:09 +0200

    NOT FIXED

--- src/components/helper/Modal/Modal.js
+++ src/components/helper/Modal/Modal.js
@@ -10,15 +10,20 @@ import {connect} from "react-redux";
 
 const modal = (props) => {
     if(props.show){
+        var showQR = false;
         const handleConfirm = event => {
             event.preventDefault();
             var cost = (props.data.cost);
             var formData = new FormData();
             formData.append("email", props.user.email);
             formData.append("points", cost);
-            subtractRequest(formData);
+            console.log(showQR);
+            if(subtractRequest(formData, showQR)){
+                console.log(showQR);
+                setTimeout(function(){showQR = false}, 3000)
+            }
         };
-        const subtractRequest = formData => {
+        const subtractRequest = (formData, showQR) => {
             axios({
                 method: 'POST',
                 url: APIURL + "/products",
@@ -29,6 +34,11 @@ const modal = (props) => {
                     console.log(json);
                     console.log(json.response);
                     if(json.data.email){
+                        if(json.data.points === props.user.points){
+                            showQR = false;
+                        }else{
+                            showQR = true;
+                        }
                         props.dispatch(changeUser(json.data));
                     } else {
                         console.log("not a user object!");
@@ -38,35 +48,36 @@ const modal = (props) => {
                     console.log(error);
                     console.log(error.response);
                 });
+            return showQR
         };
 
         return (
-            <div onClick={props.close} className="modal"
-                 style={{
-                     transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-                     opacity: props.show ? '1' : '0',
-                 }}>
+            <div onClick={props.close} className="modal">
                 <div className="modal-wrapper">
-                    <div className="modal-body"
+                    { !showQR ?
+                    <div className="modal-body noQR"
                     style={{
                         background: '#003373'
                     }}>
-                        {console.log(props.data)}
+                        <p>Weet u zeker dat u het volgende product wilt activeren?</p>
                         <p className={"bold"}>{props.data.company.name}</p>
                         <p>{props.data.product}</p>
                         <div className="modal-footer">
-                            <button className="modal-button modal-button-cancel" onClick={props.close}>Cancel</button>
-                            <button className="modal-button modal-button-confirm" onClick={handleConfirm}>Confirm</button>
+                            <button className="modal-button modal-button-cancel" onClick={props.close}>Annuleren</button>
+                            <button className="modal-button modal-button-confirm" onClick={handleConfirm}>Activeren</button>
                         </div>
                     </div>
-
-                    {/*<div className="modal-body">*/}
-                    {/*    <QRCode value={props.data.product} className="modal-qr" />*/}
-                    {/*</div>*/}
-                    {/*<div className="modal-footer">*/}
-                    {/*    <button className="modal-button modal-button-cancel" onClick={props.close}>Cancel</button>*/}
-                    {/*    <button className="modal-button modal-button-confirm" onClick={handleConfirm}>Confirm</button>*/}
-                    {/*</div>*/}
+                    :
+                    <div className="modal-body QR"
+                         style={{
+                        background: 'white'
+                    }}>
+                        <QRCode value={props.data.product} className="modal-qr" />
+                        <div className="modal-footer">
+                            <button className="modal-button modal-button-cancel" onClick={props.close}>Sluiten</button>
+                        </div>
+                    </div>
+                    }
                 </div>
             </div>
         )
